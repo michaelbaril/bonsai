a9fb3e8f8480165bf0a52a6406762bf373df33ae
Michaël Bareyt <michael.bareyt@gmail.com>
2025-11-08 14:04:00 +0100

    tests

--- tests/TreeWithCustomSetupTest.php
+++ tests/TreeWithCustomSetupTest.php
@@ -2,11 +2,13 @@
 
 namespace Baril\Bonsai\Tests;
 
+use Baril\Bonsai\Tests\Concerns\TestsCommands;
 use Baril\Bonsai\Tests\Concerns\TestsRelations;
 use Baril\Bonsai\Tests\Models\NodeWithCustomSetup;
 
 class TreeWithCustomSetupTest extends TreeTestCase
 {
+    use TestsCommands;
     use TestsRelations;
 
     protected static $defaultModelClass = NodeWithCustomSetup::class;
