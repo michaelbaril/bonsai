b0ba6c7d5bb405fce0d7f908f4173318d70e9923
Michaël Bareyt <michael.bareyt@gmail.com>
2025-11-18 21:47:00 +0100

    graft/cut

--- tests/Concerns/TestsMove.php
+++ tests/Concerns/TestsMove.php
@@ -53,6 +53,39 @@ trait TestsMove
     }
 
     /**
+     * @dataProvider graftProvider
+     */
+    public function test_graft($node, $newParent)
+    {
+        $model = $this->getModel($node);
+        $parent = $this->getModel($newParent);
+        $return = $parent->graft($model);
+
+        $this->assertModel($parent, $return);
+        $this->assertModel($parent, $model->fresh()->parent);
+    }
+
+    /**
+     * @dataProvider graftProvider
+     */
+    public function test_graft_onto($node, $newParent)
+    {
+        $model = $this->getModel($node);
+        $parent = $this->getModel($newParent);
+        $return = $model->graftOnto($parent);
+
+        $this->assertModel($model, $return);
+        $this->assertModel($parent, $model->fresh()->parent);
+    }
+
+    public static function graftProvider()
+    {
+        return array_filter(static::changeParentProvider(), function ($set) {
+            return $set[1] !== null;
+        });
+    }
+
+    /**
      * @dataProvider redundancyProvider
      */
     public function test_redundancy($node, $forbiddenParent)
@@ -71,6 +104,29 @@ trait TestsMove
             'node itself' => ['fruits rouges', 'fruits rouges'],
             'child' => ['fruits rouges', 'framboises'],
             'descendant' => ['fruits rouges', 'fraises tagada'],
+        ];
+    }
+
+    /**
+     * @dataProvider cutProvider
+     */
+    public function test_cut($node)
+    {
+        $model = $this->getModel($node);
+        $descendantsBefore = $model->descendants;
+        $return = $model->cut();
+
+        $this->assertModel($model, $return);
+        $this->assertNull($model->fresh()->parent);
+        $this->assertModels($descendantsBefore, $model->descendants());
+    }
+
+    public static function cutProvider()
+    {
+        return [
+            'already a root' => ['fruits'],
+            'regular node' => ['fruits rouges'],
+            'leaf' => ['fraises des bois'],
         ];
     }
 }
