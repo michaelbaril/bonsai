a3f293884149c1c68ae3122bca1a63dbfc90bac5
Michaël Bareyt <michael.bareyt@gmail.com>
2026-01-04 15:50:00 +0100

    for some reason SQL Server doesn't want both ONCASCADEs

--- tests/database/migrations/NodeWithUuid/0000_00_00_000001_create_node_with_uuid_tree_table.php
+++ tests/database/migrations/NodeWithUuid/0000_00_00_000001_create_node_with_uuid_tree_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\SqlServerBuilder;
 
 class CreateNodeWithUuidTreeTable extends Migration
 {
@@ -13,9 +14,18 @@ class CreateNodeWithUuidTreeTable extends Migration
      */
     public function up()
     {
-        Schema::create('node_with_uuid_tree', function (Blueprint $table) {
-            $table->foreignIdFor(\Baril\Bonsai\Tests\Models\NodeWithUuid::class, 'ancestor_id')->constrained('nodes_with_uuid')->onDelete('cascade');
-            $table->foreignIdFor(\Baril\Bonsai\Tests\Models\NodeWithUuid::class, 'descendant_id')->constrained('nodes_with_uuid')->onDelete('cascade');
+        $schema = Schema::getFacadeRoot();
+        $shouldAddConstraints = !($schema instanceof SqlServerBuilder);
+
+        Schema::create('node_with_uuid_tree', function (Blueprint $table) use ($shouldAddConstraints) {
+            $table->foreignIdFor(\Baril\Bonsai\Tests\Models\NodeWithUuid::class, 'ancestor_id')
+                ->when($shouldAddConstraints, function ($foreignId) {
+                    $foreignId->constrained('nodes_with_uuid')->onDelete('cascade');
+                });
+            $table->foreignIdFor(\Baril\Bonsai\Tests\Models\NodeWithUuid::class, 'descendant_id')
+                ->when($shouldAddConstraints, function ($foreignId) {
+                    $foreignId->constrained('nodes_with_uuid')->onDelete('cascade');
+                });
             $table->unsignedSmallInteger('depth');
 
             $table->unique(['ancestor_id', 'descendant_id']);
--- tests/database/migrations/SoftDeletableNode/0000_00_00_000001_create_soft_deletable_node_tree_table.php
+++ tests/database/migrations/SoftDeletableNode/0000_00_00_000001_create_soft_deletable_node_tree_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\SqlServerBuilder;
 
 class CreateSoftDeletableNodeTreeTable extends Migration
 {
@@ -13,9 +14,18 @@ class CreateSoftDeletableNodeTreeTable extends Migration
      */
     public function up()
     {
-        Schema::create('soft_deletable_node_tree', function (Blueprint $table) {
-            $table->foreignIdFor(\Baril\Bonsai\Tests\Models\SoftDeletableNode::class, 'ancestor_id')->constrained('soft_deletable_nodes')->onDelete('cascade');
-            $table->foreignIdFor(\Baril\Bonsai\Tests\Models\SoftDeletableNode::class, 'descendant_id')->constrained('soft_deletable_nodes')->onDelete('cascade');
+        $schema = Schema::getFacadeRoot();
+        $shouldAddConstraints = !($schema instanceof SqlServerBuilder);
+
+        Schema::create('soft_deletable_node_tree', function (Blueprint $table) use ($shouldAddConstraints) {
+            $table->foreignIdFor(\Baril\Bonsai\Tests\Models\SoftDeletableNode::class, 'ancestor_id')
+                ->when($shouldAddConstraints, function ($foreignId) {
+                    $foreignId->constrained('soft_deletable_nodes')->onDelete('cascade');
+                });
+            $table->foreignIdFor(\Baril\Bonsai\Tests\Models\SoftDeletableNode::class, 'descendant_id')
+                ->when($shouldAddConstraints, function ($foreignId) {
+                    $foreignId->constrained('soft_deletable_nodes')->onDelete('cascade');
+                });
             $table->unsignedSmallInteger('depth');
 
             $table->unique(['ancestor_id', 'descendant_id']);
